d361d6eb01c483af5b842c11f56a3a6f7624b84d
John R <pawn002@hotmail.com>
2022-05-02 10:00:13 -0400

    chore: test of chroma-js typings - out of date

--- src/app/chromajs-view/chromajs-view.component.ts
+++ src/app/chromajs-view/chromajs-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// importing all of chroma.js to speed testing
+import * as  chroma from "chroma-js";
 
 @Component({
   selector: 'app-chromajs-view',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChromajsViewComponent implements OnInit {
 
-  constructor() { }
+  // test simple oklab output
+  simpTest_Oklab(color: string) {
+
+    //  NOTE: typings not uodated for chroma-js 2.4ish as 2MAY2022@0956
+    // @ts-ignore
+    console.log(chroma(color).oklab());
+  }
+
+  constructor() {
+    this.simpTest_Oklab('#767676');
+  }
 
   ngOnInit(): void {
   }
