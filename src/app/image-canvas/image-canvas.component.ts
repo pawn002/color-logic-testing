b761777a3bf5a8ff3d1949ccab6165b0c5bf1165
John R <pawn002@hotmail.com>
2022-10-13 15:28:17 -0400

    fix: using bindings to dictate canvas size

--- src/app/image-canvas/image-canvas.component.ts
+++ src/app/image-canvas/image-canvas.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
 
+export type CanvasDimensions = {
+  width: number | undefined;
+  height: number | undefined;
+};
 @Component({
   selector: 'app-image-canvas',
   templateUrl: './image-canvas.component.html',
-  styleUrls: ['./image-canvas.component.css']
+  styleUrls: ['./image-canvas.component.css'],
 })
-export class ImageCanvasComponent implements OnInit {
+export class ImageCanvasComponent implements OnInit, AfterViewInit {
+  @Input() canvasId: string = 'canvas-element';
+  @Input() width: number = 320;
+  @Input() height: number = 240;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  ngAfterViewInit(): void {}
 }
